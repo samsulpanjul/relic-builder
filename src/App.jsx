84799112201ed6217dc80be7060bd41ebfe78c53
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2024-02-25 13:38:26 +0700

    cleaning some code

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import relic from "./utils/dataRelic";
 import { subStats } from "./utils/dataStat";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import TabsLayout from "./components/layouts/tabs/TabsLayout";
 import RelicLayout from "./components/layouts/relics/RelicLayout";
 
 function App() {
-  const [dataRelic, setDataRelic] = useState(relic);
-  const [dataSubStat, setDataSubStat] = useState(subStats);
-  const [selectedItem, setSelectedItem] = useState(null);
-
-  useEffect(() => {
-    setSelectedItem(dataRelic[0]);
-  }, []);
+  const dataRelic = relic;
+  const dataSubStat = subStats;
+  const [selectedItem, setSelectedItem] = useState(dataRelic[0]);
 
   const handleClick = (item) => {
     setSelectedItem(item);
@@ -20,7 +16,7 @@ function App() {
   return (
     <div className="flex px-10 text-slate-200 pb-[300px]">
       <TabsLayout handleClick={handleClick} dataRelic={dataRelic} />
-      <RelicLayout selectedItem={selectedItem} dataSubStat={dataSubStat} setDataSubStat={setDataSubStat} />
+      <RelicLayout selectedItem={selectedItem} dataSubStat={dataSubStat} />
     </div>
   );
 }
--- src/components/layouts/stats/StatsLayout.jsx
+++ src/components/layouts/stats/StatsLayout.jsx
@@ -4,7 +4,7 @@ import RelicSet from "../relics/RelicSet";
 import StatsDisplay from "./StatsDisplay";
 import StepCounter from "../counter/StepCounter";
 
-export default function StatsLayout({ selectedRelic, selectedMainStat, imgRelic, selectedItem, dataSubStat, handleClickRelic, handleClickMainStat, onCountStepChange, step }) {
+export default function StatsLayout({ selectedRelic, selectedMainStat, imgRelic, selectedItem, dataSubStat, handleClickRelic, handleClickMainStat }) {
   const [checkedItems, setCheckedItems] = useState(Array(dataSubStat.length).fill(false));
   const [countSteps, setCountSteps] = useState(Array(dataSubStat.length).fill(1));
   const [copied, setCopied] = useState(false);
@@ -36,95 +36,91 @@ export default function StatsLayout({ selectedRelic, selectedMainStat, imgRelic,
 
   return (
     <>
-      {selectedItem && (
+      <RelicHeader selectedItem={selectedItem} />
+      <RelicSet selectedItem={selectedItem} handleClickRelic={handleClickRelic} />
+      {selectedRelic && selectedRelic.mainStat && (
         <>
-          <RelicHeader selectedItem={selectedItem} />
-          <RelicSet selectedItem={selectedItem} handleClickRelic={handleClickRelic} />
-          {selectedRelic && selectedRelic.mainStat && (
-            <>
-              <StatsDisplay>
-                <p className="text-3xl font-bold text-center">Select Main Stat</p>
-                <div className="flex gap-2 justify-center flex-wrap">
-                  {selectedRelic.mainStat.map((item, index) => (
-                    <button key={item.name} className="text-lg rounded-lg font-bold cursor-pointer hover:bg-[#141414] bg-[#141414a9] px-3 py-2" onClick={() => handleClickMainStat(item)}>
-                      {item.name}
-                    </button>
-                  ))}
-                </div>
-              </StatsDisplay>
-              {selectedMainStat && selectedMainStat.name && (
-                <StatsDisplay>
-                  <p className="text-3xl font-bold text-center">Main Stat</p>
-                  <div className="flex justify-center place-items-center gap-5">
-                    <img src={imgRelic.img} alt={selectedRelic.name} className="w-[75px]" />
-                    <p className="text-xl font-semibold text-center">{selectedMainStat.name}</p>
-                  </div>
-                </StatsDisplay>
-              )}
-              <StatsDisplay>
-                <div className="flex justify-center gap-10">
-                  <p className="text-3xl font-bold text-center">Sub Stat</p>
-                  <button className="text-lg w-fit rounded-lg font-bold cursor-pointer hover:bg-[#141414] bg-[#141414a9] px-3 py-2" onClick={handleResetClick}>
-                    Reset
-                  </button>
-                </div>
-                <div className="flex flex-col mx-auto min-w-7/12 h-[300px] overflow-y-scroll gap-y-2 gap-x-20">
-                  {dataSubStat.map((item, index) => (
-                    <div key={item.id}>
-                      <div className="flex flex-col gap-2 justify-center">
-                        <div className="flex justify-between place-items-center gap-5 max-w-full">
-                          <div className="flex place-items-center gap-5">
-                            <input type="checkbox" className="toggle toggle-md" onChange={() => handleChecked(index)} checked={checkedItems[index]} />
-                            <p className="text-md font-semibold px-3 py-2 rounded-lg">{item.subStat}</p>
-                          </div>
-                          <StepCounter countSteps={countSteps[index]} setCountSteps={(newStep) => handleCountStepChange(index, newStep)} />
-                        </div>
+          <StatsDisplay>
+            <p className="text-3xl font-bold text-center">Select Main Stat</p>
+            <div className="flex gap-2 justify-center flex-wrap">
+              {selectedRelic.mainStat.map((item, index) => (
+                <button key={item.name} className="text-lg rounded-lg font-bold cursor-pointer hover:bg-[#141414] bg-[#141414a9] px-3 py-2" onClick={() => handleClickMainStat(item)}>
+                  {item.name}
+                </button>
+              ))}
+            </div>
+          </StatsDisplay>
+          {selectedMainStat && selectedMainStat.name && (
+            <StatsDisplay>
+              <p className="text-3xl font-bold text-center">Main Stat</p>
+              <div className="flex justify-center place-items-center gap-5">
+                <img src={imgRelic.img} alt={selectedRelic.name} className="w-[75px]" />
+                <p className="text-xl font-semibold text-center">{selectedMainStat.name}</p>
+              </div>
+            </StatsDisplay>
+          )}
+          <StatsDisplay>
+            <div className="flex justify-center gap-10">
+              <p className="text-3xl font-bold text-center">Sub Stat</p>
+              <button className="text-lg w-fit rounded-lg font-bold cursor-pointer hover:bg-[#141414] bg-[#141414a9] px-3 py-2" onClick={handleResetClick}>
+                Reset
+              </button>
+            </div>
+            <div className="flex flex-col mx-auto min-w-7/12 h-[300px] overflow-y-scroll gap-y-2 gap-x-20">
+              {dataSubStat.map((item, index) => (
+                <div key={item.id}>
+                  <div className="flex flex-col gap-2 justify-center">
+                    <div className="flex justify-between place-items-center gap-5 max-w-full">
+                      <div className="flex place-items-center gap-5">
+                        <input type="checkbox" className="toggle toggle-md" onChange={() => handleChecked(index)} checked={checkedItems[index]} />
+                        <p className="text-md font-semibold px-3 py-2 rounded-lg">{item.subStat}</p>
                       </div>
+                      <StepCounter countSteps={countSteps[index]} setCountSteps={(newStep) => handleCountStepChange(index, newStep)} />
                     </div>
-                  ))}
+                  </div>
                 </div>
-              </StatsDisplay>
-              {imgRelic && selectedMainStat && (
-                <div className="w-5/12 bg-slate-800 p-5 m-auto relative">
-                  <button className="text-sm rounded-lg px-2 py-1 hover:bg-[#141414] bg-[#141414a9] absolute top-5 right-5" onClick={handleCopyClick}>
-                    {copied ? "Copied!" : "Copy"}
-                  </button>
-                  <pre>
-                    <code ref={textAreaRef}>
-                      Id = {imgRelic.relicId},
-                      <br />
-                      Level = 15,
-                      <br />
-                      MainAffixId = {selectedMainStat.id},
-                      <br />
-                      SubAffixLists =<br />
-                      &#123;
-                      {/* // Start */}
-                      {dataSubStat.map(
-                        (item, index) =>
-                          checkedItems[index] && (
-                            <div key={item.subStat}>
-                              {"    new RelicAffix"}
-                              <br />
-                              {"    "}&#123;
-                              <br />
-                              {`        AffixId = ${item.id},`}
-                              <br />
-                              {`        Cnt = ${countSteps[index]},`}
-                              <br />
-                              {`        Step = ${countSteps[index] * 2},`}
-                              <br />
-                              {"    "}&#125;,
-                            </div>
-                          )
-                      )}
-                      {/* End */}
-                      &#125;
-                    </code>
-                  </pre>
-                </div>
-              )}
-            </>
+              ))}
+            </div>
+          </StatsDisplay>
+          {imgRelic && selectedMainStat && (
+            <div className="w-5/12 bg-slate-800 p-5 m-auto relative">
+              <button className="text-sm rounded-lg px-2 py-1 hover:bg-[#141414] bg-[#141414a9] absolute top-5 right-5" onClick={handleCopyClick}>
+                {copied ? "Copied!" : "Copy"}
+              </button>
+              <pre>
+                <code ref={textAreaRef}>
+                  Id = {imgRelic.relicId},
+                  <br />
+                  Level = 15,
+                  <br />
+                  MainAffixId = {selectedMainStat.id},
+                  <br />
+                  SubAffixLists =<br />
+                  &#123;
+                  {/* // Start */}
+                  {dataSubStat.map(
+                    (item, index) =>
+                      checkedItems[index] && (
+                        <div key={item.subStat}>
+                          {"    new RelicAffix"}
+                          <br />
+                          {"    "}&#123;
+                          <br />
+                          {`        AffixId = ${item.id},`}
+                          <br />
+                          {`        Cnt = ${countSteps[index]},`}
+                          <br />
+                          {`        Step = ${countSteps[index] * 2},`}
+                          <br />
+                          {"    "}&#125;,
+                        </div>
+                      )
+                  )}
+                  {/* End */}
+                  &#125;
+                </code>
+              </pre>
+            </div>
           )}
         </>
       )}
