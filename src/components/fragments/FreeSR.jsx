6757299ca6f3b23e5ab54420e05fd1b213ed37c2
Samsul <113889106+samsulpanjul@users.noreply.github.com>
2024-03-30 14:26:48 +0700

    robinsr

--- src/components/fragments/FreeSR.jsx
+++ src/components/fragments/FreeSR.jsx
@@ -1,40 +1,48 @@
 export default function FreeSR({ imgRelic, selectedMainStat, dataSubStat, checkedItems, countSteps, textAreaRef, copied, handleCopyClick }) {
   return (
-    <div className="w-5/12 mx-auto">
+    <div className="w-fit mx-auto">
       <pre className="dark:bg-slate-800 bg-slate-200 p-5 relative">
         <button className="text-sm font-sans rounded-lg px-2 py-1 bg-slate-300 hover:bg-slate-400 dark:hover:bg-[#141414] dark:bg-[#141414a9] absolute top-5 right-5" onClick={handleCopyClick}>
           {copied ? "Copied!" : "Copy"}
         </button>
         <code ref={textAreaRef}>
-          Id = {imgRelic.relicId},
+          BattleRelic &#123;
           <br />
-          Level = 15,
+          &#9;id= {imgRelic.relicId},
           <br />
-          MainAffixId = {selectedMainStat.id},
+          &#9;level: 15,
           <br />
-          SubAffixLists =<br />
-          &#123;
-          {/* // Start */}
-          {dataSubStat.map(
-            (item, index) =>
-              checkedItems[index] && (
-                <div key={item.subStat}>
-                  {"    new RelicAffix"}
-                  <br />
-                  {"    "}&#123;
-                  <br />
-                  {`        AffixId = ${item.id},`}
-                  <br />
-                  {`        Cnt = ${countSteps[index]},`}
-                  <br />
-                  {`        Step = ${countSteps[index] * 2},`}
-                  <br />
-                  {"    "}&#125;,
-                </div>
-              )
+          &#9;main_affix_id: {selectedMainStat.id},
+          <br />
+          {checkedItems.find((item) => item == true) && (
+            <>
+              &#9;sub_affix_list: vec!&#91;
+              <br />
+              {/* // Start */}
+              {dataSubStat.map(
+                (item, index) =>
+                  checkedItems[index] && (
+                    <div key={item.subStat}>
+                      &#9;&#9;RelicAffix &#123;
+                      <br />
+                      &#9;&#9;&#9;affix_id: {item.id},
+                      <br />
+                      &#9;&#9;&#9;cnt: {countSteps[index]},
+                      <br />
+                      &#9;&#9;&#9;step: {countSteps[index] * 2},
+                      <br />
+                      &#9;&#9;&#125;,
+                    </div>
+                  )
+              )}
+              &#9;&#93;
+              <br />
+            </>
           )}
+          &#9;{"..Default::default()"}
+          <br />
           {/* End */}
-          &#125;
+          &#125;,
         </code>
       </pre>
     </div>
